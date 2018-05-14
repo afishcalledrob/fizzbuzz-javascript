5017c635cd590ec8d796ede749344014073035f4
George Sykes <georgesykes86@gmail.com>
2018-05-14 14:20:03 +0100

    Adds fizz

---
+++ jasmine/jasmine-standalone-3/spec/fizzbuzzSpec.js
@@ -0,0 +1,5 @@
+describe("Fizzbuzz", function() {
+  it("prints fizz instead of three", function() {
+    expect(fizzBuzz(3)).toBe("fizz");
+  });
+});
---
+++ jasmine/jasmine-standalone-3/src/fizzbuzz.js
@@ -0,0 +1,3 @@
+var fizzBuzz = function(num) {
+  return "fizz";
+}
