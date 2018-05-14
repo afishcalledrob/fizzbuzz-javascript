820f99e431629f25ddb4db7a2edcf5eb6c867654
George Sykes <georgesykes86@gmail.com>
2018-05-14 14:31:56 +0100

    Finished fizzbuzz

--- jasmine/jasmine-standalone-3/spec/fizzbuzzSpec.js
+++ jasmine/jasmine-standalone-3/spec/fizzbuzzSpec.js
@@ -2,4 +2,17 @@ describe("Fizzbuzz", function() {
   it("prints fizz instead of three", function() {
     expect(fizzBuzz(3)).toBe("fizz");
   });
+
+  it("prints buzz instead of five", function() {
+    expect(fizzBuzz(5)).toBe("buzz");
+  });
+
+  it("prints the number when not divisible by 3 or 5", function() {
+    expect(fizzBuzz(7)).toBe(7);
+  });
+
+  it("prints fizzbuzz if number is multiple of 3 and 5", function() {
+    expect(fizzBuzz(15)).toBe("fizzbuzz");
+  });
+
 });
--- jasmine/jasmine-standalone-3/src/fizzbuzz.js
+++ jasmine/jasmine-standalone-3/src/fizzbuzz.js
@@ -1,3 +1,15 @@
 var fizzBuzz = function(num) {
-  return "fizz";
+  if ((num % 3 === 0) && (num % 5 === 0)) {
+    return "fizzbuzz";
+  }
+  else if (num % 5 === 0) {
+    return "buzz";
+  }
+  else if (num % 3 === 0) {
+    return "fizz";
+  }
+  else {
+    return num;
+  }
+
 }
